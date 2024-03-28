17def30ce691353c0257d4ed88cbe0bce62d5804
Tassawar hasib <nabilhasib840@gmail.com>
2024-03-28 08:01:38 +0530

    Setup peerjs library

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,4 @@
-import { useSocket } from "@/context/socket"
-import { useEffect } from "react";
-
-export default function Home() {
-
-  const socket = useSocket();
-
-  useEffect(() => {
-    socket?.on("connect", () => {
-      console.log(socket.id);
-    });
-  }, [socket])
-
+export default function Home() {  
   return (
     <h1>Welcome</h1>
   )
